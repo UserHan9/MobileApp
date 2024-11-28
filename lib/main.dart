a76b47a389d6ce356d3fe3e4b85ba293f8b63815
Userhan9 <raihanindra712@gmail.com>
2024-11-28 16:31:28 +0700

    fetch:Statless WIDGET

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("hola"),
-        centerTitle: true,
-        backgroundColor: Colors.red[500],
-      ),
-      body: const Center(
-        child: Text(
-          "WELCOME",
-          style: TextStyle(fontSize: 100),
-          ),
-      ),
-
-    floatingActionButton: FloatingActionButton(onPressed: (){
-      print("CLIK");
-    },
-      child: const Text("anjay"),
-    backgroundColor: Colors.red[600],
-     ),
-    ),
+  runApp(const MaterialApp(
+    home: Home(), 
   ));
 }
 
@@ -41,7 +22,7 @@ class Home extends StatelessWidget {
           child: Text(
             "WELCOME",
             style: TextStyle(
-              fontSize: 100,
+              fontSize: 50,
             ),
           ),
       ),
