650b5794537b11d0970b3df3c90f108448bbbc5b
Userhan9 <raihanindra712@gmail.com>
2024-12-12 15:37:32 +0700

    fetch:profile_name

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,53 @@ class FirstCard extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         elevation: 0.0, 
       ),
+      body: const Padding(padding: EdgeInsets.fromLTRB(30.0, 40.0,30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            Text("HAHA",
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0,
+              ),
+            ),
+
+          SizedBox(height: 10.0,),
+
+            Text("Muhammad Zaki",
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0,
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold
+              ),
+            ),
+
+          SizedBox(height: 30.0,),
+
+            Text("Title",
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0
+              ),
+            ),
+
+          SizedBox(height: 10.0,),
+
+            Text("Raja",
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0,
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              ),
+            ),
+
+          SizedBox(height: 30.0,)
+
+          ],
+        ),
+      ),
     );
   }
 }
